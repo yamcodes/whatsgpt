91ec157adf6608f090efc80b59f316d16063e909
yamyam263 <yamyam263@gmail.com>
2023-03-21 23:59:06 +0200

    test: :white_check_mark: Add more tests

--- tests/unit/components/HelloWorld.test.ts
+++ tests/unit/components/HelloWorld.test.ts
@@ -1,37 +1,51 @@
 import { fireEvent, render } from '@testing-library/vue';
-import { describe, expect, it } from 'vitest';
+import {
+  beforeEach, describe, expect, it,
+} from 'vitest';
 import HelloWorld from '@/components/HelloWorld.vue';
 
 const countText = (count: number) => `count is ${count}`;
+const defaultMsg = 'Hello World';
+const renderHelloWorld = (msg = defaultMsg) => render(HelloWorld, { props: { msg } });
+
+// HelloWorld Factory
+const createHelloWorld = (msg = defaultMsg) => {
+  const component = renderHelloWorld(msg);
+  return {
+    ...component,
+    getByCountText: (count: number) => component.getByText(countText(count)),
+  };
+};
 
 describe('HelloWorld.vue', () => {
+  let getByText: (text: string) => HTMLElement;
+  let getByCountText: (count: number) => HTMLElement;
+  beforeEach(() => {
+    ({ getByText, getByCountText } = createHelloWorld());
+  });
+
   it('renders a heading with a given msg', () => {
-    const msg = 'Hello World';
-    const { getByText } = render(HelloWorld, {
-      props: { msg },
-    });
-    expect(getByText(msg)).toBeTruthy();
+    expect(getByText(defaultMsg)).toBeTruthy();
   });
 
   it('should update the count when the button is clicked', async () => {
-    const { getByText } = render(HelloWorld, {
-      props: { msg: 'Hello World' },
-    });
-    const button = getByText(countText(0));
+    const button = getByCountText(0);
     await fireEvent.click(button);
     expect(button.textContent).toBe(countText(1));
   });
 
   it('should update the count when the button is clicked x100', async () => {
-    const { getByText } = render(HelloWorld, {
-      props: { msg: 'Hello World' },
-    });
-    const button = getByText(countText(0));
-
+    const button = getByCountText(0);
     await Promise.all(
       Array.from({ length: 100 }).map(() => fireEvent.click(button)),
     );
 
     expect(button.textContent).toBe(countText(100));
+  });
+
+  it('should render a heading with another msg', () => {
+    const msg2 = 'Hello World 2';
+    const { getByText: getByText2 } = createHelloWorld(msg2);
+    expect(getByText2(msg2)).toBeTruthy();
   });
 });
