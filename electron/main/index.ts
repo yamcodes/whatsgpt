6fa2c89ca620e392a3b6f676e0856a8b6810a7d2
yamyam263 <yamyam263@gmail.com>
2023-03-21 22:35:00 +0200

    refactor: fix eslint issues

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -17,18 +17,17 @@ dotenv.config();
 // ├─┬ dist
 // │ └── index.html    > Electron-Renderer
 //
-process.env.DIST_ELECTRON = join(__dirname, "..");
-process.env.DIST = join(process.env.DIST_ELECTRON, "../dist");
+process.env.DIST_ELECTRON = join(__dirname, '..');
+process.env.DIST = join(process.env.DIST_ELECTRON, '../dist');
 process.env.PUBLIC = process.env.VITE_DEV_SERVER_URL
-  ? join(process.env.DIST_ELECTRON, "../public")
+  ? join(process.env.DIST_ELECTRON, '../public')
   : process.env.DIST;
 
-
 // Disable GPU Acceleration for Windows 7
-if (release().startsWith("6.1")) app.disableHardwareAcceleration();
+if (release().startsWith('6.1')) app.disableHardwareAcceleration();
 
 // Set application name for Windows 10+ notifications
-if (process.platform === "win32") app.setAppUserModelId(app.getName());
+if (process.platform === 'win32') app.setAppUserModelId(app.getName());
 
 if (!app.requestSingleInstanceLock()) {
   app.quit();
@@ -42,15 +41,15 @@ if (!app.requestSingleInstanceLock()) {
 
 let win: BrowserWindow | null = null;
 // Here, you can also use other preload
-const preload = join(__dirname, "../preload/index.js");
+const preload = join(__dirname, '../preload/index.js');
 const devServerUrl = process.env.VITE_DEV_SERVER_URL;
 const indexHtml = join(process.env.DIST, 'index.html');
 const isDebugging = process.env.DEBUG_MODE?.toLowerCase() === 'true';
 
 async function createWindow() {
   win = new BrowserWindow({
-    title: "Main window",
-    icon: join(process.env.PUBLIC, "favicon.ico"),
+    title: 'Main window',
+    icon: join(process.env.PUBLIC, 'favicon.ico'),
     webPreferences: {
       preload,
       // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
@@ -71,26 +70,26 @@ async function createWindow() {
   } else win.loadFile(indexHtml);
 
   // Test actively push message to the Electron-Renderer
-  win.webContents.on("did-finish-load", () => {
-    win?.webContents.send("main-process-message", new Date().toLocaleString());
+  win.webContents.on('did-finish-load', () => {
+    win?.webContents.send('main-process-message', new Date().toLocaleString());
   });
 
   // Make all links open with the browser, not with the application
   win.webContents.setWindowOpenHandler(({ url }) => {
-    if (url.startsWith("https:")) shell.openExternal(url);
-    return { action: "deny" };
+    if (url.startsWith('https:')) shell.openExternal(url);
+    return { action: 'deny' };
   });
   // win.webContents.on('will-navigate', (event, url) => { }) #344
 }
 
 app.whenReady().then(createWindow);
 
-app.on("window-all-closed", () => {
+app.on('window-all-closed', () => {
   win = null;
-  if (process.platform !== "darwin") app.quit();
+  if (process.platform !== 'darwin') app.quit();
 });
 
-app.on("second-instance", () => {
+app.on('second-instance', () => {
   if (win) {
     // Focus on the main window if the user tried to open another
     if (win.isMinimized()) win.restore();
@@ -98,14 +97,14 @@ app.on("second-instance", () => {
   }
 });
 
-app.on("activate", () => {
+app.on('activate', () => {
   const allWindows = BrowserWindow.getAllWindows();
   if (allWindows.length) allWindows[0].focus();
   else createWindow();
 });
 
 // New window example arg: new windows url
-ipcMain.handle("open-win", (_, arg) => {
+ipcMain.handle('open-win', (_, arg) => {
   const childWindow = new BrowserWindow({
     webPreferences: {
       preload,
